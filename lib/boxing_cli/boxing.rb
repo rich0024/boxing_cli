4577131c18696a76378918e3c248bc331dd540dd
Richard Yance <yance0022@gmail.com>
2019-03-13 16:32:37 +0000

    planning

---
+++ lib/boxing_cli/boxing.rb
@@ -0,0 +1,20 @@
+class BoxingCli::Boxing
+
+  attr_accessor :name, :rank, :division, :age, :record
+  @@all = []
+
+  puts "Hello! Welcome to pound for pound!"
+  puts "To see the top 15 pound for pound fighters, type 'rankings'"
+  puts "To view the fighter's division, age and record, enter the fighter's rank"
+
+  def rankings
+    #prints out ranking of fighters 1-25
+    #isolate fighter name and add index
+    #personlink.each_with_index{ |val, index| puts "#{index}. #{val}"
+  end
+
+  def
+
+  end
+
+end
